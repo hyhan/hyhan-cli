1bad0571fe0a7437560f72d8c101b3013d833bc5
hui <125173679@qq.com>
2022-04-22 16:56:29 +0800

    init

---
+++ packages/core/lib/core.js
@@ -0,0 +1,8 @@
+'use strict'
+const utils = require('@hyhan-cli/utils')
+module.exports = core
+
+function core() {
+  // TODO
+}
+console.log(utils.add(1, 2, 3))
---
+++ packages/utils/lib/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+  add,
+}
+
+function add(x, y, z) {
+  return x + y + z
+}
